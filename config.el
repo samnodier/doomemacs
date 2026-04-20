0442ce83e95ccc5e0220796394e21900bac8fe53
Sam Nodier <sam@samnodier.com>
2026-04-20 14:19:47 -0400

    add org mode support for tasks

--- config.el
+++ config.el
@@ -5,13 +5,42 @@
 (setq display-line-numbers-type 'relative)
 (setq org-directory "~/org/")
 
+;; Task management
+(after! org
+  ;; Files the agenda watches
+  (setq org-agenda-files '("~/org/inbox.org"
+                           "~/org/projects.org"
+                           "~/org/someday.org"))
+
+  ;; Todo states with handy shortcuts
+  (setq org-todo-keywords
+        '((sequence "TODO(t)" "NEXT(n)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c)")))
+
+  ;; Where captured tasks land by default
+  (setq org-default-notes-file "~/org/inbox.org")
+
+  ;; Capture templates
+  (setq org-capture-templates
+        '(("t" "Task" entry
+           (file "~/org/inbox.org")
+           "* TODO %?\n:PROPERTIES:\n:CREATED: %U\n:END:\n")
+          ("n" "Next action" entry
+           (file "~/org/inbox.org")
+           "* NEXT %?\n:PROPERTIES:\n:CREATED: %U\n:END:\n")
+          ("j" "Journal / note" entry
+           (file+datetree "~/org/journal.org")
+           "* %?\n%U\n"))))
+
 (after! evil
   (setq evil-escape-key-sequence "jk"
         evil-escape-delay 0.2))
 
 (setq +format-on-save-enabled-modes t)
 
-(map! :n "-" #'dired-jump)
+(map! :n "-" #'dired-jump
+      :leader
+      "x" #'org-capture
+      "X" #'doom/open-scratch-buffer)
 
 (after! eglot
   (add-to-list 'eglot-server-programs
